029f16fef4a942a2ef3c8bcf3d089b7b5cf83957
adamakadem <adamakadem@gmail.com>
2025-05-05 03:13:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
 @app.route("/api/make_order", methods=['POST'])
 def make_order(data=None):
     try:
+        log_helper.log.info('Информация по заказу\n')
         if data:
             req_data = data
         else:
